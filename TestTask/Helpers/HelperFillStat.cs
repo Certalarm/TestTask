1613c677f4a6393c15c663dd6cef050ca1d7d4aa
Eugene <github@certalarm.ru>
2026-02-11 08:37:37 +0800

    Перенес логику заполнения статистики в HelperFillStat.

---
+++ TestTask/Helpers/HelperFillStat.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace TestTask.Helpers
+{
+    public static class HelperFillStat
+    {
+        public static void FillSingleStatMap(char c, Dictionary<string, LetterStats> map)
+        {
+            if (IsGoodSingleChar(c))
+            {
+                FillStatMap(BuildSingleKey(c), map);
+            }
+        }
+
+        public static void FillDoubleStatMap(char c1, char c2, Dictionary<string, LetterStats> map)
+        {
+            if (IsGoodDoubleChar(c1, c2))
+            {
+                FillStatMap(BuildDoubleKey(c1, c2), map);
+            }
+        }
+
+        private static string BuildSingleKey(char c) => c.ToString();
+
+        private static string BuildDoubleKey(char c1, char c2) => $"{c1}{c2}".ToUpper();
+
+        private static bool IsGoodSingleChar(char c) =>
+            char.IsLetter(c);
+
+        private static bool IsGoodDoubleChar(char c1, char c2) =>
+            IsLetterBoth(c1, c2) && IsEquallyBoth(c1, c2);
+
+        private static bool IsLetterBoth(char c1, char c2) =>
+            char.IsLetter(c1) && char.IsLetter(c2);
+
+        private static bool IsEquallyBoth(char c1, char c2) =>
+            char.ToUpperInvariant(c1) == char.ToUpperInvariant(c2);
+
+        private static void FillStatMap(string key, Dictionary<string, LetterStats> map)
+        {
+            if (map.TryGetValue(key, out LetterStats existingStat))
+            {
+                IncStatistic(existingStat);
+            }
+            else
+            {
+                map[key] = new LetterStats
+                {
+                    Letter = key,
+                    Count = 1
+                };
+            }
+        }
+
+        /// <summary>
+        /// Метод увеличивает счётчик вхождений по переданной структуре.
+        /// </summary>
+        /// <param name="letterStats"></param>
+        private static void IncStatistic(LetterStats letterStats)
+        {
+            letterStats.Count++;
+        }
+    }
+}
--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Globalization;
+using TestTask.Helpers;
 
 namespace TestTask
 {
@@ -63,7 +62,7 @@ namespace TestTask
             {
                 char c = stream.ReadNextChar();
                 // TODO : заполнять статистику с использованием метода IncStatistic. Учёт букв - регистрозависимый. (+)
-                FillSingleStatMap(c, letterCountsMap);
+                HelperFillStat.FillSingleStatMap(c, letterCountsMap);
             }
             return new List<LetterStats>(letterCountsMap.Values);
         }
@@ -84,7 +83,7 @@ namespace TestTask
             {
                 char c = stream.ReadNextChar();
                 // TODO : заполнять статистику с использованием метода IncStatistic. Учёт букв - НЕ регистрозависимый. (+)
-                FillDoubleStatMap(prevC, c, pairCountsMap);    
+                HelperFillStat.FillDoubleStatMap(prevC, c, pairCountsMap);    
                 prevC = c;
             }
             return new List<LetterStats>(pairCountsMap.Values);
@@ -121,67 +120,6 @@ namespace TestTask
         {
             // TODO : Выводить на экран статистику. Выводить предварительно отсортировав по алфавиту!
             throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Метод увеличивает счётчик вхождений по переданной структуре.
-        /// </summary>
-        /// <param name="letterStats"></param>
-        private static void IncStatistic(LetterStats letterStats)
-        {
-            letterStats.Count++;
-        }
-
-
-
-
-
-        private static void FillSingleStatMap(char c, Dictionary<string, LetterStats> map)
-        {
-            if (IsGoodSingleChar(c))
-            {
-                FillStatMap(BuildSingleKey(c), map);
-            }
-        }
-
-        private static void FillDoubleStatMap(char c1, char c2, Dictionary<string, LetterStats> map)
-        {
-            if (IsGoodDoubleChar(c1, c2))
-            {
-                FillStatMap(BuildDoubleKey(c1, c2), map);
-            }
-        }
-
-        private static string BuildSingleKey(char c) => c.ToString();
-
-        private static string BuildDoubleKey(char c1, char c2) => $"{c1}{c2}".ToUpper();
-
-        private static bool IsGoodSingleChar(char c) =>
-            char.IsLetter(c);
-
-        private static bool IsGoodDoubleChar(char c1, char c2) =>
-            IsLetterBoth(c1, c2) && IsEquallyBoth(c1, c2);
-
-        private static bool IsLetterBoth(char c1, char c2) =>
-            char.IsLetter(c1) && char.IsLetter(c2);
-
-        private static bool IsEquallyBoth(char c1, char c2) =>
-            char.ToUpperInvariant(c1) == char.ToUpperInvariant(c2);
-
-        private static void FillStatMap(string key, Dictionary<string, LetterStats> map)
-        {
-            if (map.TryGetValue(key, out LetterStats existingStat))
-            {
-                IncStatistic(existingStat);
-            }
-            else
-            {
-                map[key] = new LetterStats
-                {
-                    Letter = key,
-                    Count = 1
-                };
-            }
         }
     }
 }
