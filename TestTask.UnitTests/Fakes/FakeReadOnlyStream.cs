cb2a8cf0bd402b09f183d685fccec3a4a6e638b2
Eugene <github@certalarm.ru>
2026-02-11 09:03:43 +0800

    Создал FakeReadOnlyStream.

---
+++ TestTask.UnitTests/Fakes/FakeReadOnlyStream.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.UnitTests.Fakes
+{
+    public class FakeReadOnlyStream : IReadOnlyStream
+    {
+        public bool IsEof => throw new NotImplementedException();
+
+        #region .ctors
+        public FakeReadOnlyStream(string inputData)
+        {
+            
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public char ReadNextChar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetPositionToStart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
